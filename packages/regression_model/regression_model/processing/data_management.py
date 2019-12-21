1d16501c1308f3ccc6e05bed4d5197598984fc97
zahid1202 <zahid1202@gmail.com>
2019-12-20 18:55:08 -0800

    differential_tests

--- packages/regression_model/regression_model/processing/data_management.py
+++ packages/regression_model/regression_model/processing/data_management.py
@@ -1,3 +1,31 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@zahid1202 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+0
+0839zahid1202/deploying-machine-learning-models
+forked from trainindata/deploying-machine-learning-models
+ Code Pull requests 0 Actions Projects 0 Wiki Security Insights Settings
+deploying-machine-learning-models/packages/regression_model/regression_model/processing/data_management.py
+@ChristopherGS ChristopherGS Section 9.3 - Differential Tests in CI Part 1
+5a0382f on Jan 26
+61 lines (44 sloc)  1.83 KB
+  
+Code navigation is available!
+Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
+
+ Code navigation is available for this repository but data for this commit does not exist.
+
+Learn more or give us feedback
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
@@ -6,6 +34,7 @@ from regression_model.config import config
 from regression_model import __version__ as _version
 
 import logging
+import typing as t
 
 
 _logger = logging.getLogger(__name__)
@@ -19,7 +48,6 @@ def load_dataset(*, file_name: str
 
 def save_pipeline(*, pipeline_to_persist) -> None:
     """Persist the pipeline.
-
     Saves the versioned model, and overwrites any previous
     saved models. This ensures that when the package is
     published, there is only one trained model that can be
@@ -30,7 +58,7 @@ def save_pipeline(*, pipeline_to_persist) -> None:
     save_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
     save_path = config.TRAINED_MODEL_DIR / save_file_name
 
-    remove_old_pipelines(files_to_keep=save_file_name)
+    remove_old_pipelines(files_to_keep=[save_file_name])
     joblib.dump(pipeline_to_persist, save_path)
     _logger.info(f'saved pipeline: {save_file_name}')
 
@@ -44,14 +72,17 @@ def load_pipeline(*, file_name: str
     return trained_model
 
 
-def remove_old_pipelines(*, files_to_keep) -> None:
+def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
     """
     Remove old model pipelines.
     This is to ensure there is a simple one-to-one
     mapping between the package version and the model
     version to be imported and used by other applications.
+    However, we do also include the immediate previous
+    pipeline version for differential testing purposes.
     """
-
+    do_not_delete = files_to_keep + ['__init__.py']
     for model_file in config.TRAINED_MODEL_DIR.iterdir():
-        if model_file.name not in [files_to_keep, '__init__.py']:
+        if model_file.name not in do_not_delete:
             model_file.unlink()
+
--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -1,3 +1,4 @@
+Learn more or give us feedback
 import math
 
 from regression_model.predict import make_prediction
@@ -7,10 +8,10 @@ from regression_model.processing.data_management import load_dataset
 def test_make_single_prediction():
     # Given
     test_data = load_dataset(file_name='test.csv')
-    single_test_json = test_data[0:1]
+    single_test_input = test_data[0:1]
 
     # When
-    subject = make_prediction(input_data=single_test_json)
+    subject = make_prediction(input_data=single_test_input)
 
     # Then
     assert subject is not None
@@ -22,10 +23,10 @@ def test_make_multiple_predictions():
     # Given
     test_data = load_dataset(file_name='test.csv')
     original_data_length = len(test_data)
-    multiple_test_json = test_data
+    multiple_test_input = test_data
 
     # When
-    subject = make_prediction(input_data=multiple_test_json)
+    subject = make_prediction(input_data=multiple_test_input)
 
     # Then
     assert subject is not None
